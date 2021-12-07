8c2c940f9560a9ff1597440838efa151650c33aa
jHiuzak <62451511+jHiuzak@users.noreply.github.com>
2021-12-07 11:23:06 -0300

    Banco completo para a primeira fase do projeto

---
+++ database/migrations/2021_12_07_141853_create_problems_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProblemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('problems', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+			$table->string('title');
+			$table->string('address');
+			$table->string('photo');
+			$table->string('description');
+			$table->timestamp('starting_date');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('problems');
+    }
+}
