ea3a2d7450b2af4aa8e11f2162a0c2eadaa8b1f1
WhiteWolf <aldf.wolfbrleo1803@gmail.com>
2022-01-17 17:55:13 -0300

    Adição do Breeze

--- routes/web.php
+++ routes/web.php
@@ -1,54 +1,24 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Http\Request;
 
-use App\Http\Controllers\ProblemasController;
-use \App\Models\User;
-use \App\Models\Problema;
-
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
 
 Route::get('/', function () {
-    $problemas = Problema::all();
+    return view('welcome');
+});
 
-    return view('feed', ['problemas' => $problemas]);
-})->name('feed');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
-Route::get('/login', function () {
-    return view('login');
-})->name('login');
-
-Route::post('/login/load', function (Request $req) {
-    $email = $req->post('email');
-    $senha = $req->post('senha');
-
-    return redirect()->route('feed', []);
-})->name('login.load');
-
-Route::get('/cadastro', function () {
-    return view('cadastro');
-})->name('cadastro');
-
-Route::post('/cadastro/load', function (Request $req) {
-    $nome = $req->post('nome');
-    $email = $req->post('email');
-    $senha = $req->post('senha');
-    $data = $req->post('data');
-    $telefone = $req->post('telefone');
-
-    return redirect()->route('feed', []);
-})->name('cadastro.load');
-
-Route::get('/problemaDetalhes', function () {
-    $problema = (object)array();
-    $problema->data = "10/01/2022";
-    $problema->endereco = "Rua Ronaldinho";
-    $problema->titulo = "Buraco";
-    $problema->descricao = "eee";
-
-
-    return view ('problemaDetalhes', ['problema' => $problema]);
-})->name('problema.detalhes');
-
-Route::get('/problema', [ProblemasController::class, 'create'])->name('problema');
-Route::post('/problema/criar', [ProblemasController::class, 'store'])->name('problema.criar');
+require __DIR__.'/auth.php';
