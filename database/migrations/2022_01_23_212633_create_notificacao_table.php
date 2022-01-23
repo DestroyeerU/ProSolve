5e04046ea91da97f7f7c134cba4b4f534ec86082
WhiteWolf <aldf.wolfbrleo1803@gmail.com>
2022-01-23 19:16:30 -0300

    Tabela de Notificações

---
+++ database/migrations/2022_01_23_212633_create_notificacao_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNotificacaoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notificacao', function (Blueprint $table) {
+            $table->id();
+            $table->foreign('usuario_destino')->references('id')->on('usuarios');
+            $table->foreign('publicacao')->references('id')->on('problemas');
+            $table->string('mensagem');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('notificacao');
+    }
+}
