387790abf520679058e9c60bfcfe7fc9c5e73901
WhiteWolf <aldf.wolfbrleo1803@gmail.com>
2022-01-23 15:42:35 -0300

    Atualização na Autenticação

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -19,7 +19,7 @@
         <h1>ProSolve</h1>
       </div>
       <div class="login-box">
-        <form class="login-form" action="{{ route('login.load') }}" method="POST">
+        <form class="login-form" action="{{ url('/login') }}" method="POST">
           @csrf
           <h3 class="login-head"><i class="fa fa-lg fa-fw fa-user"></i>Login</h3>
           <div class="form-group">
