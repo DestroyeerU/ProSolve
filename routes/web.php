7bd8b0cc428f8bc5da51b00fe3fb057defbce9f2
WhiteWolf <aldf.wolfbrleo1803@gmail.com>
2022-01-23 15:46:52 -0300

    Atualização na Autenticação

--- routes/web.php
+++ routes/web.php
@@ -14,20 +14,12 @@ Route::get('/', function () {
     return view('feed', ['problemas' => $problemas]);
 })->name('feed');
 
-Route::get('/login', function () {
-    return view('login');
-})->name('login');
-
 Route::post('/login/load', function (Request $req) {
     $email = $req->post('email');
     $senha = $req->post('senha');
 
     return redirect()->route('feed', []);
 })->name('login.load');
-
-Route::get('/cadastro', function () {
-    return view('cadastro');
-})->name('cadastro');
 
 Route::post('/cadastro/load', function (Request $req) {
     $nome = $req->post('nome');
