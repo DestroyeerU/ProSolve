e42445862fa37b503c9cb9e4240634a05883ba7a
WhiteWolf <aldf.wolfbrleo1803@gmail.com>
2022-01-26 19:31:56 -0300

    Correção de Rotas

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -20,7 +20,7 @@ class RegisteredUserController extends Controller
      */
     public function create()
     {
-        return view('layout.cadastro');
+        return view('cadastro');
     }
 
     /**
